5dfdeb64a0cba0993975c59938671a353ed6875a
SaldanhaElisson <62814287+SaldanhaElisson@users.noreply.github.com>
2021-02-19 23:39:40 -0300

    Update js.js

--- js.js
+++ js.js
@@ -12,3 +12,31 @@ const modal = {
         
     }
 }
+
+const transactions =[{
+    id:1,
+    description: 'luz',
+    amount:-50000,
+    date='23/01/2021'
+},{
+    id:2,
+    description: 'criação de website',
+    amount:50000,
+    date='23/01/2021'},{
+        id:3,
+        description: 'internet',
+        amount:-2000,
+        date='23/01/2021'}]
+
+const transaction ={
+    income(){
+        //numero da entradas
+    },
+    amount(){
+        // numeros de sáidas
+    },
+    total(){
+        // total income - amount
+    }
+    
+}
